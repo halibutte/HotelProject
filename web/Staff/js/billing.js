e854a146b3264301303f94c6c24b302c201085db
x3041557 <x3041557@cmpmsc-20.cmp.uea.ac.uk>
2017-12-10 18:26:10 +0000

    Minor change

--- web/Staff/js/billing.js
+++ web/Staff/js/billing.js
@@ -12,3 +12,13 @@ function update_price(event) {
     price = parseFloat(price).toFixed(2);
     $(selElem).closest("form").find("[name='item_price']").val(price);
 }
+function highlight_updated() {
+    var highlight = $("#guest_container").data("highlightchild");
+    if(!isNaN(highlight)) {
+        var el = $("#guest_container").find("[data-highlightnum="+highlight+"]");
+        $(el).addClass("message-confirm");
+        setTimeout(function() {
+            $(el).removeClass("message-confirm");
+        }, 1500);
+    }
+}
